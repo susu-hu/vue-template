3f68c45c31966276f552648a6c1ee0891a4792d4
susu <1628469970@qq.com>
2022-05-19 22:08:21 +0800

    vue-pdf

--- vue_demo/src/views/plug/reviewer.vue
+++ vue_demo/src/views/plug/reviewer.vue
@@ -2,7 +2,7 @@
  * @Author: susu 1628469970@qq.com
  * @Date: 2022-03-27 15:20:31
  * @LastEditors: susu 1628469970@qq.com
- * @LastEditTime: 2022-05-18 10:03:34
+ * @LastEditTime: 2022-05-19 22:06:20
  * @FilePath: \vue_demo\src\views\plug\reviewer.vue
  * @Description: 文件预览
 -->
@@ -48,7 +48,7 @@ export default {
       src: "",
       numPages: 0, // 总页数
       curPageNum: 0, //当前页
-      url: "https://susu-file.oss-cn-beijing.aliyuncs.com/susu/2.20%E5%85%AC%E5%BC%80%E8%AF%BE%E5%A4%A7%E7%BA%B2%281%29.pdf",
+      url: "https://susu-file.oss-cn-beijing.aliyuncs.com/susu/%E4%B8%80%E4%BA%9B%E7%BD%91%E7%AB%99%E4%BB%AC.pdf",
     };
   },
   methods: {
@@ -65,13 +65,13 @@ export default {
   },
   mounted() {
     let url =
-      "https://susu-file.oss-cn-beijing.aliyuncs.com/susu/2.20%E5%85%AC%E5%BC%80%E8%AF%BE%E5%A4%A7%E7%BA%B2%281%29.pdf";
+      "https://susu-file.oss-cn-beijing.aliyuncs.com/susu/%E4%B8%80%E4%BA%9B%E7%BD%91%E7%AB%99%E4%BB%AC.pdf";
     this.$refs.detail.scrollIntoView(true);
     this.src = pdf.createLoadingTask(url); // url是pdf文件的全路径
     this.src.promise.then((pdf) => {
       console.log(pdf);
       this.numPages = pdf.numPages;
-      // that.pageCount = pdf.numPages;
+      // this.pageCount = pdf.numPages;
     });
   },
 };
