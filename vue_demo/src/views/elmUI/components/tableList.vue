872b0e2e11b02c43251c799720b984ab19186e4a
susu <1628469970@qq.com>
2022-07-20 21:29:25 +0800

    fix

--- vue_demo/src/views/elmUI/components/tableList.vue
+++ vue_demo/src/views/elmUI/components/tableList.vue
@@ -1,18 +1,42 @@
 <!--
- * @Author: 胡苏珍 1628469970@qq.com
  * @Description: table列表
 -->
 <template>
-  <el-table :data="data" style="width: 100%" height="100%" stripe v-loading="loading">
+  <el-table
+    :data="data"
+    style="width: 100%"
+    height="100%"
+    stripe
+    v-loading="loading"
+  >
     <template v-for="(item, index) in list">
       <template v-if="item.type">
-        <el-table-column :type="item.type" :width="item.width" :key="index" :fixed="item.fixed" :label="item.label" />
+        <el-table-column
+          :type="item.type"
+          :width="item.width"
+          :key="index"
+          :fixed="item.fixed"
+          :label="item.label"
+        />
       </template>
       <template v-else>
-        <el-table-column :width="item.width" :key="index" :fixed="item.fixed" :sortable="item.sortable" :show-overflow-tooltip="item.showtip" :label="item.label" :prop="item.prop">
+        <el-table-column
+          :width="item.width"
+          :key="index"
+          :fixed="item.fixed"
+          :sortable="item.sortable"
+          :show-overflow-tooltip="item.showtip"
+          :label="item.label"
+          :prop="item.prop"
+        >
           <template slot-scope="{ row, $index }" v-if="!item.type">
             <!-- 自定义插槽 -->
-            <slot v-if="item.slot" :name="item.slot" :row="row" :index="$index"></slot>
+            <slot
+              v-if="item.slot"
+              :name="item.slot"
+              :row="row"
+              :index="$index"
+            ></slot>
             <!-- 空数据--显示 -->
             <span v-else>{{ row[item.prop] }}</span>
           </template>
