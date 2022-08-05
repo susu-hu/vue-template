faf820d26bf7c9b1f1c9bf0e13534587e6db2d69
susu <1628469970@qq.com>
2022-08-05 23:09:26 +0800

    feat:"vue-seamless-scroll的使用"

---
+++ vue_demo/src/views/plug/seamlesScroll.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: susu 1628469970@qq.com
+ * @Date: 2022-08-05 23:04:50
+ * @LastEditors: susu 1628469970@qq.com
+ * @LastEditTime: 2022-08-05 23:08:40
+ * @FilePath: \vue_demo\src\views\plug\seamlesScroll.vue
+ * @Description: "vue-seamless-scroll"
+ * @link:https://chenxuan0000.github.io/vue-seamless-scroll/
+-->
+
+<template>
+  <div class="contentBox"></div>
+</template>
+
+<script>
+// npm install vue-seamless-scroll --save
+export default {};
+</script>
+
+<style></style>
