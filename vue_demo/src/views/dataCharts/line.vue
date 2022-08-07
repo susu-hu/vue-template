98c27df0558085c81b9b074d37d522cefe376a9b
susu <1628469970@qq.com>
2022-08-08 00:25:16 +0800

    feat:"流动特效"

--- vue_demo/src/views/dataCharts/line.vue
+++ vue_demo/src/views/dataCharts/line.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="contentBox">
+  <div class="flex contentBox">
     <div id="chart" ref="chart" class="chart"></div>
+    <div id="chart2" ref="chart2" class="chart"></div>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ export default {
   data() {
     return {
       charts: null,
+      charts2: null,
     };
   },
   mounted() {
@@ -20,17 +22,28 @@ export default {
         renderer: "canvas",
       });
     }
-    this.initData5();
+    if (this.charts2 == null) {
+      this.charts2 = echarts.init(document.getElementById("chart2"), null, {
+        renderer: "canvas",
+      });
+    }
+    setTimeout(() => {
+      this.initData7();
+      this.initData8();
+    }, 60);
     erd.listenTo(document.getElementById("chart"), this.handleWindowResize);
+    erd.listenTo(document.getElementById("chart2"), this.handleWindowResize);
   },
   beforeDestroy() {
-    erd.uninstall(this.$refs.lineCharts);
+    erd.uninstall(this.$refs.chart);
+    erd.uninstall(this.$refs.chart2);
   },
   methods: {
     handleWindowResize() {
-      if (!this.charts) return;
+      if (!this.charts || !this.chart2) return;
       this.$nextTick(() => {
         this.charts.resize();
+        this.charts2.resize();
       });
     },
     initData() {
@@ -934,14 +947,361 @@ export default {
       };
       this.charts.setOption(option);
     },
-    initData7() {},
+    initData7() {
+      let yData = [222, 932, 66, 934, 111, 333, 0],
+        xData = ["测1", "测2", "测3", "测4", "测5", "测6", "测7"],
+        datacoords = [
+          {
+            coords: [],
+          },
+        ];
+      for (var i = 0; i < xData.length; i++) {
+        datacoords[0].coords.push([xData[i], yData[i]]);
+      }
+      this.charts.setOption({
+        animation: true, //控制动画示否开启
+        animationDuration: 3000,
+        animationEasing: "bounceOut", //缓动动画
+        animationThreshold: 8, //动画元素的阈值
+        backgroundColor: "transparent", // 给echarts图设置背景色
+        tooltip: {
+          trigger: "axis",
+          backgroundColor: "rgba(0,0,0,.5)",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "rgba(0,0,0,.5)",
+            },
+          },
+          textStyle: {
+            color: "#fff",
+            fontSize: 14,
+          },
+        },
+        grid: {
+          left: "3%", //图表距边框的距离
+          right: "3%",
+          top: "15%",
+          bottom: "5%",
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            nameGap: 3,
+            nameTextStyle: {
+              color: "rgba(255,255,255,.8)",
+              fontSize: 12,
+            },
+            type: "category",
+            data: xData,
+            boundaryGap: false, //从0开始
+            axisLine: {
+              onZero: true,
+              rotate: 30, //坐标轴内容过长旋转
+              interval: 1,
+              lineStyle: {
+                color: "#636E7C",
+              },
+            },
+            axisLabel: {
+              color: "rgba(255,255,255,.8)", //坐标的字体颜色
+              fontSize: 12,
+            },
+            axisTick: {
+              //坐标轴刻度颜色  x和y不交叉
+              show: false,
+            },
+          },
+        ],
+        yAxis: [
+          {
+            name: "个",
+            min: 0,
+            max: function (value) {
+              return Math.ceil(value.max / 5) * 5;
+            },
+            splitNumber: 5,
+            type: "value",
+            nameTextStyle: {
+              color: "rgba(255,255,255,.89)",
+              fontSize: 12,
+            },
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: "rgba(255,255,255,.25)",
+                type: "dashed",
+              },
+            },
+            axisTick: {
+              //坐标轴刻度颜色
+              show: false,
+            },
+            axisLine: {
+              //坐标轴线颜色
+              show: true,
+              lineStyle: {
+                color: "#636E7C",
+              },
+            },
+            axisLabel: {
+              color: "rgba(255,255,255,.8)", //坐标的字体颜色
+              fontSize: 12,
+            },
+          },
+        ],
+        series: [
+          {
+            name: "苏苏小苏苏",
+            type: "line",
+            smooth: false,
+            lineStyle: {
+              color: "#DC7828",
+              width: 1.5,
+              type: "dashed",
+              shadowOffsetX: 0, // 折线的X偏移
+              shadowOffsetY: 10, // 折线的Y偏移
+              shadowBlur: 4, // 折线模糊
+              shadowColor: "rgba(255, 255, 255, 0.8)", //设置折线阴影颜色
+            },
+            showSymbol: true, //是否默认展示圆点
+            symbol: "circle", // 默认是空心圆（中间是白色的）
+            symbolSize: 7,
+            itemStyle: {
+              color: "#021E47", //实圆的背景色
+              borderWidth: 1,
+              borderColor: "#DC7828",
+            },
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
+                {
+                  offset: 1,
+                  color: "rgba(220,120,40,0.8)",
+                },
+                {
+                  offset: 0.74,
+                  color: "rgba(220,120,40,0.5)",
+                },
+                {
+                  offset: 0,
+                  color: "rgba(220,120,40,0)",
+                },
+              ]),
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: yData,
+          },
+          {
+            showSymbol: false,
+            name: "苏苏小苏苏",
+            type: "lines",
+            polyline: true,
+            smooth: false,
+            coordinateSystem: "cartesian2d",
+            zlevel: 1,
+            effect: {
+              show: true,
+              smooth: true,
+              period: 6,
+              symbolSize: 4,
+            },
+            lineStyle: {
+              color: "#fff",
+              width: 1,
+              opacity: 0,
+              curveness: 0,
+              cap: "round",
+            },
+            data: datacoords,
+          },
+        ],
+      });
+    },
+    initData8() {
+      let yData = [90, 555, 666, 999, 567, 999, 888, 0],
+        xData = ["测1", "测2", "测3", "测4", "测5", "测6", "测7", "测8"],
+        datacoords = [];
+      for (var i = 0; i < xData.length; i++) {
+        datacoords.push([
+          {
+            coord: [i, yData[i]],
+          },
+          {
+            coord: [i + 1, yData[i + 1]],
+          },
+        ]);
+      }
+      this.charts2.setOption({
+        animation: true, //控制动画示否开启
+        animationDuration: 3000,
+        animationEasing: "bounceOut", //缓动动画
+        animationThreshold: 8, //动画元素的阈值
+        tooltip: {
+          trigger: "axis",
+          backgroundColor: "rgba(0,0,0,.5)",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "rgba(0,0,0,.5)",
+            },
+          },
+          textStyle: {
+            color: "#fff",
+            fontSize: 14,
+          },
+        },
+        grid: {
+          left: "3%", //图表距边框的距离
+          right: "3%",
+          top: "15%",
+          bottom: "5%",
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            nameGap: 3,
+            nameTextStyle: {
+              color: "rgba(255,255,255,.8)",
+              fontSize: 12,
+            },
+            type: "category",
+            data: xData,
+            boundaryGap: false, //从0开始
+            axisLine: {
+              onZero: true,
+              rotate: 30, //坐标轴内容过长旋转
+              interval: 1,
+              lineStyle: {
+                color: "#636E7C",
+              },
+            },
+            axisLabel: {
+              color: "rgba(255,255,255,.8)", //坐标的字体颜色
+              fontSize: 12,
+            },
+            axisTick: {
+              //坐标轴刻度颜色  x和y不交叉
+              show: false,
+            },
+          },
+        ],
+        yAxis: [
+          {
+            name: "个",
+            min: 0,
+            max: function (value) {
+              return Math.ceil(value.max / 5) * 5;
+            },
+            splitNumber: 5,
+            type: "value",
+            nameTextStyle: {
+              color: "rgba(255,255,255,.89)",
+              fontSize: 12,
+            },
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: "rgba(255,255,255,.25)",
+                type: "dashed",
+              },
+            },
+            axisTick: {
+              //坐标轴刻度颜色
+              show: false,
+            },
+            axisLine: {
+              //坐标轴线颜色
+              show: true,
+              lineStyle: {
+                color: "#636E7C",
+              },
+            },
+            axisLabel: {
+              color: "rgba(255,255,255,.8)", //坐标的字体颜色
+              fontSize: 12,
+            },
+          },
+        ],
+        series: [
+          {
+            name: "苏苏小苏苏",
+            type: "line",
+            smooth: false,
+            lineStyle: {
+              color: "aqua",
+              width: 1.5,
+              type: "solid",
+              shadowOffsetX: 0, // 折线的X偏移
+              shadowOffsetY: 0, // 折线的Y偏移
+              shadowBlur: 4, // 折线模糊
+              shadowColor: "rgba(255, 255, 255, 0.8)", //设置折线阴影颜色
+            },
+            showSymbol: true, //是否默认展示圆点
+            symbol: "circle", // 默认是空心圆（中间是白色的）
+            symbolSize: 7,
+            itemStyle: {
+              color: "#021E47", //实圆的背景色
+              borderWidth: 1,
+              borderColor: "aqua",
+            },
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
+                {
+                  offset: 1,
+                  color: "rgba(0, 255, 255, .7)",
+                },
+                {
+                  offset: 0.74,
+                  color: "rgba(0, 170, 255, 0.3)",
+                },
+                {
+                  offset: 0,
+                  color: "rgba(220,120,40,0)",
+                },
+              ]),
+            },
+            emphasis: {
+              focus: "series",
+            },
+            data: yData,
+          },
+          {
+            showSymbol: false,
+            name: "苏苏小苏苏",
+            type: "lines",
+            polyline: true,
+            smooth: false,
+            coordinateSystem: "cartesian2d",
+            zlevel: 1,
+            effect: {
+              show: true,
+              smooth: true,
+              period: 2,
+              symbolSize: 4,
+            },
+            lineStyle: {
+              color: "#fff",
+              width: 10,
+              opacity: 0,
+              curveness: 0,
+              cap: "round",
+            },
+            data: datacoords,
+          },
+        ],
+      });
+    },
   },
 };
 </script>
 <style scoped lang="less">
+.contentBox {
+  background: linear-gradient(90deg, #03224e 0%, #011030 100%);
+}
 .chart {
-  width: 100%;
-  height: 600px;
-  color: transparent;
+  width: 600px;
+  height: 400px;
 }
 </style>
